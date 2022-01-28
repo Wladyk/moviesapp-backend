7af8fe4a935f015d60b53405c83bc36f5b659401
Wladyk <cristianmitas@gmail.com>
2022-01-28 03:30:41 -0300

    Initial settings. Models and admin panel pages.

---
+++ moviesbackend/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(User)
+admin.site.register(WatchLater)
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(Rating)
---
+++ moviesbackend/models.py
@@ -0,0 +1,55 @@
+from django.db import models 
+from django.utils.timezone import now
+from django.contrib.auth.models import AbstractUser
+import datetime
+
+#The Genre model will be used to provide a customizable list of gendres for each movie
+class Genre(models.Model):
+    name = models.CharField(max_length=200, default="No name")
+    description = models.TextField(default="No description")
+    def __str__(self):
+        return str(self.id) + "-" + self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200, default="No title")
+    releaseDate = models.DateField(default=now)
+    genre = models.ForeignKey(Genre, on_delete=models.RESTRICT) #we cannot destroy genres
+    #id there're still movies assigned to them
+    plot = models.TextField(default="No plot")
+    averageRating = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.id) + "-" + self.title 
+
+#new user model has been defined to account for the "Watch later list"
+class User(AbstractUser):
+    watchLaterList = models.ManyToManyField(Movie, through="WatchLater")
+    def __str__(self):
+        return str(self.id) +"-"+ self.username
+
+#this "intersection" model provides the link between "User" and "Movie"
+# we could use the implicit model created by Django on the ManyToManyField
+#but I wanted to add date
+class WatchLater(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    dateSet = models.DateTimeField(default=now)
+    def __str__(self):
+        return str(self.id)+"-"+self.user.username+"-"+self.movie.title
+
+#Ratings may be given to a movie by a certain user
+class Rating(models.Model):
+    class GivenRate(models.IntegerChoices):
+        HORRIBLE = 1
+        BAD = 2
+        ACCEPTABLE = 3
+        GOOD = 4
+        EXCELLENT = 5
+    rate = models.IntegerField(choices=GivenRate.choices)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    #if an author gets deleted, the ratings she/he gave will still remain
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    #deleting a movie will also delete all the ratings associated to it
+    message = models.TextField(default="No comments")
+    date = models.DateTimeField(default=now)
+    def __str__(self):
+        return "Rating ID: " + str(self.id)
